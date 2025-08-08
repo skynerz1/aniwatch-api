3eb6a92c721a38bfe66a192d734a7cfea9257489
skynerz1 <153457906+skynerz1@users.noreply.github.com>
2025-08-08 08:10:53 +0300

    Update logging.ts

--- src/middleware/logging.ts
+++ src/middleware/logging.ts
@@ -3,7 +3,10 @@ import { logger as honoLogger } from "hono/logger";
 import { log } from "../config/logger.js";
 
 export const logging: MiddlewareHandler = honoLogger(
-    (msg: string, ...rest: string[]) => {
-        log.info(msg, ...rest);
+  (msg?: string, ...rest: unknown[]) => {
+    if (msg) {
+      // تأكد rest كلها يمكن طباعتها أو استعملها حسب حاجتك
+      log.info(msg, ...rest);
     }
+  }
 );
